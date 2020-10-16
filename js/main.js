6fc2a586147b7fab719cf1c30eb489d3bcfa3458
marija.gud <2001marija2001@gmail.com>
2020-10-16 11:25:42 +0300

    php

---
+++ js/main.js
@@ -0,0 +1,71 @@
+let knygos =
+    [
+        {
+            'istorija': [
+
+
+
+                {
+                    ISBN: '9786098120462',
+                    leidimoMetai: 2019,
+                    pavadinimas: 'Karalienės Bonos virtuvė',
+                    puslapiuSkaicius: '220'
+                }
+                ,
+                {
+                    ISBN: "9771392155005",
+                    leidimoMetai: 2015,
+                    pavadinimas: 'Kultūros paminklai 23',
+                    puslapiuSkaicius: '208'
+                }
+                ,
+                {
+                    ISBN: '9786094674044',
+                    leidimoMetai: 2019,
+                    pavadinimas: 'Lietuvos kultūros istorija',
+                    puslapiuSkaicius: '356'
+                }
+                ,
+                {
+                    ISBN: '9786094664373',
+                    leidimoMetai: 2016,
+                    pavadinimas: 'Kai Kaunas buvo Kaunas',
+                    puslapiuSkaicius: '200'
+                }
+            ]
+        },
+        {
+            'fantastika': [
+                {
+                    ISBN: '9786094273902',
+                    leidimoMetai: 2019,
+                    pavadinimas: 'Amžinybės fjordo pranašai',
+                    puslapiuSkaicius: '496'
+                }
+                ,
+                {
+                    ISBN: '9786094273872',
+                    leidimoMetai: 2017,
+                    pavadinimas: 'Fondas ir imperija',
+                    puslapiuSkaicius: '328'
+                }
+                ,
+                {
+                    ISBN: '9789955138303',
+                    leidimoMetai: 2019,
+                    pavadinimas: 'Sulaikyti žvaigždes',
+                    puslapiuSkaicius: '336'
+                }
+                ,
+                {
+                    ISBN: '9786090135631',
+                    leidimoMetai: 2018,
+                    pavadinimas: 'Smėlis',
+                    puslapiuSkaicius: '336'
+                }
+            ]
+        },
+
+        for (let kategorija of){
+
+        }
